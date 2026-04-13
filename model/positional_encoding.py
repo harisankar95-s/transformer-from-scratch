3add7e80770a74f5b6730dd1492718ded39cde41
Hari <hsrvhari2112@gmail.com>
2026-04-13 21:40:48 +0530

    Positional encoding

---
+++ model/positional_encoding.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self,d_model,max_seq_length):
+        super().__init__()
+        self.d_model = d_model
+        self.max_seq_length = max_seq_length
+        self.positional_encoding = torch.zeros(max_seq_length,d_model)
+        self.position = torch.arange(0, max_seq_length).unsqueeze(1).float()
+        self.pair_length = self.d_model//2
+        self.column_pair = torch.arange(0,self.pair_length)
+        self.two_i = (self.column_pair*2).float()
+        self.div_term = torch.pow(10000, self.two_i / self.d_model)
+
+
+
+
+
