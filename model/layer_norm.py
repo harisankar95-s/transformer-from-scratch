a13485d79d40db74d2dc0f9d924f6bba1dc7469b
harisankar95-s <hsrvhari2112@gmail.com>
2026-04-18 18:54:01 +0530

    Add layer normalization component

---
+++ model/layer_norm.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
